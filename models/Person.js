1f1a7144b4a29606ec814a583e7d5aecb6b799d7
aman <dasaman713@gmail.com>
2024-05-30 16:46:42 +0000

    office work 2nd day

--- models/Person.js
+++ models/Person.js
@@ -1,46 +1,77 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-
-const personSchema= new mongoose.Schema({
+// Define the Person schema
+const personSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     },
     age:{
-        type: Number,
-
+        type: Number
     },
     work:{
-        type:String,
-        enum: ['chef','waiter', 'manager'],
+        type: String,
+        enum: ['chef', 'waiter', 'manager'],
         required: true
     },
     mobile:{
-        type:String,
+        type: String,
         required: true
     },
     email:{
-        type:String,
+        type: String,
         required: true,
         unique: true
     },
     address:{
-        type:String,
+        type: String
     },
     salary:{
-        type:Number,
+        type: Number,
         required: true
     },
-    username:{
-        type:String,
-        required: true
+    username: {
+        required: true,
+        type: String
     },
-    password:{
-        type:String,
-        required: true
+    password: {
+        required: true,
+        type: String
+    }
+});
+
+personSchema.pre('save', async function(next){
+    const person = this;
+
+    // Hash the password only if it has been modified (or is new)
+    if(!person.isModified('password')) return next();
+
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+        
+        // Override the plain password with the hashed one
+        person.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
     }
 })
 
-const Person= mongoose.model('Person',personSchema);
+personSchema.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
 
-module.exports=Person
+// Create Person model
+const Person = mongoose.model('Person', personSchema);
+module.exports = Person;
--- server.js
+++ server.js
@@ -1,56 +1,36 @@
-const express = require("express");
-
-
+const express = require('express')
 const app = express();
-const db=require('./db');
-const MenuItem= require('./models/MenuItem');
-const Person =require('./models/Person');
-const bodyParser = require("body-parser");
-const passport= require('passport');
-const LocalStrategy=require('passport-local').Strategy;
-// const Person =require('./')
+const db = require('./db');
+require('dotenv').config();
+const passport = require('./auth');
 
-app.use(bodyParser.json());
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
-const logRequest=(req,res,next)=>{
-    console.log(`${new Date().toLocaleDateString()} Request Made to: ${req.originalUrl}`);
-    next();
+
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
 }
+app.use(logRequest);
 
-
-passport.use(new LocalStrategy(async(Username,password, done)=>{
-    try{
-        console.log("recieved credential:", Username, password);
-        const user=await Person.findOne({username:Username});
-        if(!user){
-            return done(null, false,{message:"Incorrect Username"});
-        }
-
-        const isPasswordMatch=user.password===password?true:false;
-
-        if(isPasswordMatch){
-            return done(null,user);
-        }else{
-            return done(null, false,{message:"Incorrect Username"});
-        }
-    }catch(err){
-        return done(err);
-    }
-}))
 app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false})
 
-const PORT = 3000;
-const localAuthMiddleware= passport.authenticate('local', {session: false});
-app.get('/', localAuthMiddleware,(req, res) => {
-    res.send('welcone to my hotel');
-});
+app.get('/',localAuthMiddleware, function (req, res) {
+    res.send('Welcome to our Hotel');
+})
 
+// Import the router files
+const personRoutes = require('./routes/personRoutes');
+const menuItemRoutes = require('./routes/menuItemRoutes');
 
-
-const personRoutes=require('./routes/personRoutes');
-app.use('/person',localAuthMiddleware,personRoutes);
-
-const menuRoutes=require('./routes/menuItemRoutes');
-app.use('/menu',menuRoutes);
-
-app.listen(PORT, () => console.log(`Server Started at Port${PORT}`));
+// Use the routers
+app.use('/person', personRoutes);
+app.use('/menu', menuItemRoutes);
+  
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
